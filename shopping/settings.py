f8f5a8831c009e6fb7e5f5b2f57ebe9471366733
sunnygupta460 <sunnygupta460@gmail.com>
2019-01-31 12:39:44 +0530

    pull code

--- shopping/settings.py
+++ shopping/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
----------------------------------------------------
-MWS_ACCESS_KEY = AKIAJYPN3FHMFSH3ZTOQ
-MWS_SECRET_KEY = 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
-MWS_ACCOUNT_ID = 892517388586
+# ---------------------------------------------------
+# MWS_ACCESS_KEY = AKIAJYPN3FHMFSH3ZTOQ
+# MWS_SECRET_KEY = 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
+# MWS_ACCOUNT_ID = 892517388586"
