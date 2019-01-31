7d776e32042c79a3323792c7a8efe0c2cd674c07
sunnygupta460 <sunnygupta460@gmail.com>
2019-01-31 17:35:12 +0530

    code pushed

--- order/management/commands/get_product.py
+++ order/management/commands/get_product.py
@@ -1,21 +1,24 @@
 from django.core.management.base import BaseCommand
 import mws, os
 from product.models import Product
+from shopping import settings
 
 class Command(BaseCommand):
 
 	def handle(self, *args, **options):
+
 		product_api = mws.Products(
 			access_key=os.environ['MWS_ACCESS_KEY'],
 			secret_key=os.environ['MWS_SECRET_KEY'],
 			account_id=os.environ['MWS_ACCOUNT_ID'],
 			region='IN',
 			)
-		
 		service_status = product_api.get_service_status()
 		service_status
 		service_status.original
 		service_status.parsed
 		service_status.response
+		print(service_status.parsed)
 
 		# Create the entry in the product model
+
--- order/management/commands/get_store.py
+++ order/management/commands/get_store.py
@@ -6,17 +6,17 @@ from shopping import settings
 class Command(BaseCommand):
 
 	def handle(self, *args, **options):
-		store_api = mws.Stores(
+		store_api = mws.Store(
 			access_key=settings.MWS_ACCESS_KEY,
 			secret_key=settings.MWS_SECRET_KEY,
 			account_id=settings.MWS_ACCOUNT_ID,
 			region='IN',
 			)
-		
 		service_status = store_api.get_service_status()
 		service_status
 		service_status.original
 		service_status.parsed
 		service_status.response
-
+		print(service_status.parsed)
+		
 		# Create the entry in the store model
